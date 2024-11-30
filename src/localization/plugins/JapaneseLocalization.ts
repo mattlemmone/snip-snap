8e58ccbc9dead61f70a1a17ed2308ebe0563c516
mattlemmone <mattlemmone@gmail.com>
2024-11-30 10:46:20 +0900

    rename consts -> locales

--- src/localization/plugins/JapaneseLocalization.ts
+++ src/localization/plugins/JapaneseLocalization.ts
@@ -1,5 +1,5 @@
 import { BaseLocalizationStrategy } from '../BaseLocalizationStrategy';
-import { SUPPORTED_LOCALES } from '../../localization/constants';
+import { SUPPORTED_LOCALES } from '../../localization/locales';
 
 export default class JapaneseLocalization extends BaseLocalizationStrategy {
   languageCode = SUPPORTED_LOCALES.JAPANESE;
